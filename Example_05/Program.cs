f63241dd9b09742b1b49a1d93f4726012c7ab239
Dmitry-Chisotv <111231261+Dmitry-Chisotv@users.noreply.github.com>
2022-08-20 20:16:44 +0300

    ivan

---
+++ Example_05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "иван")
+{
+    Console.WriteLine("Ура, это же Ванюха Пельмеха! Быстро убирай игрушки и собирайся! Мама заждалась!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+} 
